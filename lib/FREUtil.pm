9b65b9b30a357fd00b532edccd3c4469a7ec44a3
Carlo Rosati <carlo.rosati@noaa.gov>
2015-09-01 10:38:19 -0400

    Perl...

--- lib/FREUtil.pm
+++ lib/FREUtil.pm
@@ -856,58 +856,29 @@ sub optionValuesListParse($$@)
   }
 }
 
-sub decodeSystemExitStatus($$)
-# ------ arguments: $childError $osError
-# ------ returns (decodedStatus, errorMessage)
-# ------ Uses knowledge of Unix headers sys/wait.h and bits/waitstatus.h to decode Perl system
-# ------ call exit status. The Perl system call exit status mimics status returned by wait and
-# ------ waitpid. The return value is 16 bits where the exit status is the 8 high order bits,
-# ------ whether or not a core was dumped is the highest of the 8 low order bits and the seven
-# ------ remaining low order bits contain the signal number.
-# ------ See the Perl documentation for system() for more information.
+sub decodeErrorStatus($$)
+# ------ arguments: $? $!
+# ------ returns decoded child process native error status in string form 
+# ------ See Perl's documentation for system() for more information.
 {
-  my ($childError, $osError) = @_;
+  my ($child_error, $os_error) = @_;
 
-  my ($decodedStatus, $errorMessage);
+  my $error_string;
 
-  # Check if Perl system call failed to execute
-  if ($childError == -1)
+  if ($child_error == -1)
   {
-    $decodedStatus = -1;
-
-    $errorMessage = "failed to execute: $osError\n";
+    $error_string = "failed to execute: $os_error";
   }
-
-  # Check if system call terminated by signal
-  elsif ($childError & 127)
+  elsif ($child_error & 127)
   {
-    $errorMessage = "child died ";
-
-    if ($childError & 128)
-    {
-      $errorMessage .= "with";
-    }
-    else
-    {
-      $errorMessage .= "without";
-    }
-
-    # Get the signal from the low order 7 bits
-    $decodedStatus = $childError & 127;
-
-    $errorMessage .= " coredump with signal $decodedStatus\n";
+    $error_string = "child terminated for signal " . ($child_error & 127);
   }
-
-  # Otherwise, return the system call exit status
   else
   {
-    # Get the exit status from the high order 8 bits
-    $decodedStatus = $childError >> 8;
-
-    $errorMessage = "child exited with value $decodedStatus\n";
+    $error_string = "child exited with status " . ($child_error >> 8);
   }
 
-  return ($decodedStatus, $errorMessage);
+  return $error_string . "\n";
 }
 
 # //////////////////////////////////////////////////////////////////////////////
