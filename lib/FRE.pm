555ea92d96f7e90f8da1a96418a9ea0f9c2177a1
Chris Blanton <Chris.Blanton@gaea8.ncrc.gov>
2015-11-04 11:47:34 -0500

    first stab at moving remote platform csh to site file added new gfdl/env.defaults file added new frelist option --fre-version that reads the new platform FRE version. added capability to frerun to use env.defaults file as default csh remote environment maintaining XML for overrides added fre "class" property, freVersion that contains the platform FRE version removed shell vs xml fre version checking for now

--- lib/FRE.pm
+++ lib/FRE.pm
@@ -379,20 +379,6 @@ sub new($$%)
                             if ($properties) {
                                 $properties->propertiesList( $o{verbose} );
 
-                                # check for FRE version mismatch between XML and current shell
-                                my @loaded_fre_modules = grep s#fre/##, split ':', $ENV{LOADEDMODULES};
-                                # verify only one fre module is loaded
-                                if ((my $n = scalar @loaded_fre_modules) != 1) {
-                                    FREMsg::out( $o{verbose}, FREMsg::FATAL,
-                                        "$n FRE modules appear to be loaded; should be 1" );
-                                    return '';
-                                }
-                                if ((my $shell = $loaded_fre_modules[0]) ne (my $xml = $properties->{FRE_VERSION})) {
-                                    FREMsg::out( $o{verbose}, FREMsg::FATAL,
-                                        "FRE version mismatch between shell ($shell) and XML ($xml)" );
-                                    return '';
-                                }
-
 # ----------------------------------------------- locate the platform node (no backward compatibility anymore)
                                 my $platformNode = $platformNodeGet->($rootNode);
                                 if ($platformNode) {
@@ -419,6 +405,7 @@ sub new($$%)
 # ---------------------------------------------------------------------------- calculate and save misc values in the object
                                     $fre->{project} = $projectGet->( $fre, $o{project} );
                                     $fre->{baseCsh} = $fre->platformValue('csh');
+                                    $fre->{freVersion} = $fre->platformValue('freVersion');
 
 # -------------------------------------------------------------------------------------------------- derive the mkmf template
                                     my $mkmfTemplate = $mkmfTemplateGet->(
