7fc77e0ad2f5875fcf13da28bb1beaa4d0350e72
Carlo Rosati <carlo.rosati@noaa.gov>
2017-04-11 09:59:12 -0400

    test_helpers: Use new string compare function

--- t/FRE_tests/test_helpers.bash
+++ t/FRE_tests/test_helpers.bash
@@ -2,13 +2,16 @@
 unique_stdout_xml() {
     awk </dev/null '/platform/{s=0}/"'"${FRE_SYSTEM_SITE}"'.intel"/{s=1}{if (s) {if ($0 ~ /<directory/){system("sed s/UNIQUE_STRING/FRE_tests-'"${unique_string}"'-temp/ '"${platform}"'.dirs")} else {print}} else {print}}' "$@"
 }
+string_matches_pattern() {
+    [ $( expr "$1" : "$2" ) -gt 0 ]
+}
 remove_output_matching() {
     output=$(printf '%s' "$output" | grep -v "$1")
 }
 remove_lines_matching() {
     for k in "${!lines[@]}"
     do
-        if [ $( expr "${lines[$k]}" : "$1" ) -gt 0 ]
+        if [ $( string_matches_pattern "${lines[$k]}" "$1" ) ]
         then
             unset lines[$k]
         fi
@@ -18,7 +21,4 @@ remove_ninac_from_output_and_lines() {
     local ninac="NiNaC'ing ...Dir([.]*done\!)"
     remove_lines_matching "$ninac"
     remove_output_matching "$ninac"
-}
-string_matches_pattern() {
-    [ $( expr "$1" : "$2" ) -gt 0 ]
 }
